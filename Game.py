9557eaadee089a4f017742ff017de96db465f61a
PoluriRamkumar <166031028+PoluriRamkumar@users.noreply.github.com>
2024-04-07 15:38:25 +0530

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,77 @@
+import random
+
+hangman1 = [
+"""
++---+
+    |
+    |
+    |
+    ===""", """
++---+
+  | |
+  O |
+    |
+    ===""", """
++---+
+  | |
+  O |
+ /| |
+    ===""", """
++---+
+  | |
+  O |
+ /|\|
+    ===""", """
++---+
+  | |
+  O |
+ /|\|
+  |  ===""", """
+ / 
++---+
+  | |
+  O |
+ /|\|
+  |  ===""", """
+ / \
+"""
+]
+
+animals = ['tiger', 'lion', 'giraffe', 'zebra', 'dolphin', 'shark','elephant', 'rhino']
+
+word =random.choice(animals).lower()
+
+guessed_correctly = []
+guessed_incorrectly = []
+tries = 6
+hangman_count = -1
+while tries > 0:
+    output = ''
+    for letter in word:
+        if letter in guessed_correctly:
+            output += letter
+        else:
+            output += '_ '
+            if output == word:
+                break
+            
+            
+            print("Guess the word: ",output)
+            print(tries, "chances left")
+            guess = input().lower()
+            if guess in guessed_correctly or guess in guessed_incorrectly:
+                print("Already guessed", guess)
+            elif guess in word:
+                print("Awesome job! You guessed a correct letter!")
+            else:
+                print("Sorry! You have guessed a wrong letter!")
+                hangman_count = hangman_count + 1
+                tries = tries - 1
+                guessed_incorrectly.append(guess)
+                
+                
+                if tries>0:
+                    print("You guessed it right and you win!!!")
+                else:
+                    print("Sorry you guessed the wrong letter. try again.")
+                    
